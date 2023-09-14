01c95dde565575e286fbb03885f0531734873591
Phenolah <phenolaha@gmail.com>
2023-09-14 05:57:58 -0700

    Updated views.py

--- ProfileApp/views.py
+++ ProfileApp/views.py
@@ -1,5 +1,25 @@
-
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Person
+from .serializers import PersonSerializer
+
+class PersonCreateView(generics.CreateAPIView):  # Use CreateAPIView for POST requests
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PersonRetriveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+'''from .models import Person
 from rest_framework import generics
 from .serializers import PersonSerializer
 
@@ -11,4 +31,4 @@ class PersonCreateView(generics.ListCreateAPIView):
     serializer_class = PersonSerializer
 class PersonRetriveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    serializer_class = PersonSerializer'''
